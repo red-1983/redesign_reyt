2fd1e9172221c5597f974ec95d76e4b8786b3c61
Yura <benzayura@tut.by>
2025-11-06 00:00:24 +0300

    feat: add new section about services

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,6 @@
 export { default as Geo } from "./icon/Geo";
 export { default as Cloak } from "./icon/Cloak";
 export { default as Tel } from "./icon/Tel";
+export { default as servicesImage2 } from "./img/services/2.webp";
+export { default as servicesImage3 } from "./img/services/3.webp";
+export { default as servicesImage4 } from "./img/services/4.webp";
---
+++ src/components/ui/card/Card.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+import { Button } from "@/components/ui";
+import clsx from "clsx";
+
+interface CardProps {
+  className?: string;
+  h3?: string;
+  image?: string | StaticImageData;
+  altImage?: string;
+  href?: string;
+}
+export const Card = ({
+  className,
+  h3,
+  image,
+  altImage,
+  href = "#",
+  ...props
+}: CardProps) => {
+  return (
+    <li
+      className={clsx(
+        `${className} group`,
+        "flex h-[325px] w-[310px] cursor-pointer flex-col items-center justify-between",
+        className
+      )}
+      {...props}
+    >
+      <div className="flex w-full flex-col items-center gap-2">
+        {" "}
+        <Link href={href} className="w-full overflow-hidden">
+          {image && altImage && (
+            <Image
+              src={image}
+              alt={altImage}
+              className="min-h-[200px] w-[300px] transition-transform duration-300 ease-in-out group-hover:scale-110"
+            />
+          )}
+        </Link>
+        {h3 && <h3 className="text-center">{h3}</h3>}
+      </div>
+      <Button
+        variant="secondary"
+        className="bg-brand-components h-[50px] w-[300px]"
+      >
+        Подробнее
+      </Button>
+    </li>
+  );
+};
